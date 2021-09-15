3888ba6e9a2b7b5751b2b3bcbde04b442bd88db3
oomoto <oomoto@ryoei.jp>
2021-09-15 15:06:45 +0900

    setup
    react app

---
+++ src/components/tables/Table.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TableRow from "./TableRow";
+
+function Table() {
+    return (
+        <div className="Table">
+            <TableRow />
+            <TableRow />
+            <TableRow />
+        </div>
+    )
+}
+
+export default Table
---
+++ src/components/tables/TableRow.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function TableRow() {
+    return (
+        <div className="Row">
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <input type="text" />
+            <button type="button">3</button>
+            <input type="text" />
+            
+        </div>
+    )
+}
+
+export default TableRow;
