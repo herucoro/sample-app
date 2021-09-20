0031418430b15419fe7cbc13521bca5459b0b0c9
oomoto <oomoto@ryoei.jp>
2021-09-20 17:19:41 +0900

    add
    ui wijmo

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Sample from './components/pages/sample';
-import SampleHeader from './components/organisms/header';
+import '@grapecity/wijmo.styles/wijmo.css';
+import '@grapecity/wijmo.cultures/wijmo.culture.ja';
+import * as wjGrid from '@grapecity/wijmo.react.grid.multirow';
+import * as wjInput from '@grapecity/wijmo.react.input'
+import { getData } from './data';
 
 function App() {
+  const appData = getData()
+
   return (
     <div className="App">
-      <SampleHeader />
-      <Sample />
+      
+      <wjGrid.MultiRow itemsSource={appData.data}  layoutDefinition={appData.lines}>
+      </wjGrid.MultiRow>
     </div>
   );
 }
---
+++ src/data.tsx
@@ -0,0 +1,85 @@
+import * as wjCore from '@grapecity/wijmo'
+import * as wjGrid from '@grapecity/wijmo.grid'
+import { CellMaker } from '@grapecity/wijmo.grid.cellmaker';
+// import { appDataType } from './interfaces'
+
+export type gridType = {
+    materialCode: number,
+    merchandiseName: string,
+    manufacturerNumber: string,
+    quantity: number,
+    unit: {number: number, name: string},
+    price: number,
+    total: number,
+    deliveryDate: string,
+    usedDate: string,
+    attachedCount: number,
+    remake: string,
+    stickynote: string
+}
+
+export interface appDataType {
+    data: any,
+    lines: any
+}
+
+export function getData() {
+    const appData: appDataType = {data:'', lines:''}
+    const detail: gridType[] = []
+
+    for (let i = 0; i < 12; i++) {
+        detail.push({
+            materialCode: 61122000 + i,
+            merchandiseName: '押釦_TWX0001_東西南北',
+            manufacturerNumber: 'TWX0001_WESN',
+            quantity: 1234,
+            unit:{number: 1, name: '式'},
+            price: 1234567.00,
+            total: 1234567,
+            deliveryDate: '21/09/11',
+            usedDate: '21/09/11',
+            attachedCount: 3,
+            remake: '押釦の私用は添付ファイル参照',
+            stickynote: '廃判になっており確認中'
+        })
+    }
+
+    appData.data = new wjCore.CollectionView(detail)
+    appData.lines = [
+        {
+            header: '資材ヘッダ', colspan: 1, cells: [
+                { binding: 'materialCode', header: '資材CD'}
+            ]
+        },
+        {
+            header: '資材明細', colspan: 5, cells: [
+                { binding: 'merchandiseName', header: '商品名'},
+                { binding: 'quantity', header: '数量', colspan: 2},
+                { binding: 'price', header: '単価'},
+                { binding: 'deliveryDate', header: '希望納期'},
+                { binding: 'manufacturerNumber', header: 'メーカー希望納期', width: 500},
+                { binding: 'unit.number', header: '単位数'},
+                { binding: 'unit.name', header: '単位名'},
+                { binding: 'total', header: '金額'},
+                { binding: 'usedDate', header: '使用日'}
+            ]
+        },
+        {
+            header: '添付', colspan: 1, cells: [
+                { binding: 'attachedCount', header: '添付', cellTemplate: CellMaker.makeButton({
+                    click: (e, etx) => {
+                        alert('ボタンが押されました');
+                    }
+                }) , width:50}
+            ]
+        },
+        {
+            header: '資材詳細', colspan: 1, cells: [
+                { binding: 'remake', header: '備考'},
+                { binding: 'stickynote', header: '付箋', width:500}
+            ]
+        }
+    ]
+
+    return appData
+}
